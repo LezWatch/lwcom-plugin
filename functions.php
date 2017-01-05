a33a53c9f01aeff75254afe06c24e40af9a276bb
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2017-01-04 18:54:36 -0800

    First pass

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: Base Functions
+Plugin URI:  https://lezwatchcommercials.com
+Description: This file calls all the children files, depending on what other
+Version: 1.0
+Author: Mika Epstein
+*/
+
+// Include CPTs
+include( 'cpts/commercials.php' );

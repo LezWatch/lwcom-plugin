d14ee146696d55e1ae454851b4caaa52d2dbbb9e
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2017-07-29 09:41:42 -0700

    Ensure this only runs on commercials

--- functions.php
+++ functions.php
@@ -7,6 +7,23 @@ Version: 1.0
 Author: Mika Epstein
 */
 
+// First make sure this only runs on the right sites
+$site_url    = parse_url( get_site_url() );
+$valid_sites = array( 'lezwatchcommercials.com', 'com.lezpress.local', 'lezwatchcommercials.dev', 'lezwatchcommercials.local' );
+
+function lwcom_plugin_deactivation() {
+	global $valid_sites;
+    echo '<div id="message" class="error"><p>';
+    echo 'This plugin can only be run on LezWatch Commercials. You\'re using <strong>' . get_site_url() . '</strong>. If you\'re trying to run this on a local site, tell Mika your URL or use one of these: ' . implode( ", ", $valid_sites ) .'.';
+    echo '</p></div>';
+}
+
+if ( !in_array( $site_url['host'], $valid_sites ) ) {
+	deactivate_plugins( plugin_basename( __FILE__ ) );
+	add_action( 'admin_notices', 'lwcom_plugin_deactivation' );
+    return;
+}
+
 // Include CPTs
 include( 'cpts/commercials.php' );
 
